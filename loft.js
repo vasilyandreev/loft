3d3ec28e977c53fb99f3727fe380849476c06898
Alexei Andreev <alexei.andreev@gmail.com>
2014-12-06 21:05:53 -0600

    Can only trophy once a day.

--- loft.js
+++ loft.js
@@ -9,18 +9,36 @@ if (Meteor.isServer) {
 	Meteor.startup(function () {
 		// code to run on server at startup
 	});
+
+	// Called when a user is created.
+	Accounts.onCreateUser(function(options, user) {
+		// We still want the default hook's 'profile' behavior.
+		if (options.profile) {
+			user.profile = options.profile;
+		}
+		user["loft"] = { lastTrophyTime: 0 };
+		return user;
+	});
 }
 
 // This code only runs on the client
 if (Meteor.isClient) {
 	Meteor.subscribe("posts");
+	Session.set('loginError', "");
+	Session.set('registerError', "");
+	Meteor.call("canTrophy", function(err, result) {
+		Session.set('canTrophy', result);
+	});
+	Meteor.call("getDebugTrophy", function(err, result) {
+		console.log(result);
+	});
 
+	// BODY
 	Template.body.helpers({
 		posts: function () {
 			return posts.find({}, {sort: {createdAt: -1}});
 		}
 	});
-
 	Template.body.events({
 		"submit .new-post": function (event) {
 			console.log("submit .new-post");
@@ -37,12 +55,23 @@ if (Meteor.isClient) {
 		}
 	});
 
+	// POST
+	Template.post.helpers({
+		'canTrophy' : function() {
+			return Session.get('canTrophy');
+		},
+	});
 	Template.post.events({
 		"click .trophy-button": function () {
-			Meteor.call("addTrophy", this._id);
+			Meteor.call("addTrophy", this._id, function (err, result) {
+				if (result) {
+					Session.set('canTrophy', false);
+				}
+			});
 		}
 	});
 
+	// LOGIN
 	Template.login.events({
 		'submit #login-form' : function(e, t){
 			e.preventDefault();
@@ -74,7 +103,7 @@ if (Meteor.isClient) {
 			var password = t.find('#account-password').value;
 			var profile = {
 				firstName: t.find('#account-first-name').value,
-				lastName: t.find('#account-last-name').value
+				lastName: t.find('#account-last-name').value,
 			};
 
 			// TODO: Trim and validate the input
@@ -88,7 +117,6 @@ if (Meteor.isClient) {
 					// has logged in successfully. 
 					Session.set('registerError', "");
 					Session.set('loginError', "");
-					//Meteor.call("setUserName", Meteor.userId(), firstName, lastName);
 				}
 			});
 			return false;
@@ -105,12 +133,7 @@ if (Meteor.isClient) {
 }
 
 Meteor.methods({
-	setUserName: function (userId, firstName, lastName) {
-		if (Meteor.userId() != userId) {
-			throw new Meteor.Error("Invalid userId.");
-		}
-		Meteor.users.update(userId, { $set: { profile : { firstName: firstName, lastName: lastName } } });
-	},
+	// Create a new post with the given text.
 	addPost: function (text) {
 		if (!Meteor.userId()) {
 			throw new Meteor.Error("Not authorized.");
@@ -120,15 +143,49 @@ Meteor.methods({
 			userId: Meteor.userId(),
 			name: profile.firstName + " " + profile.lastName,
 			text: text,
-			createdAt: new Date(),
+			createdAt: Date.now(),
 			trophiesBy: [] // list of userIds who have given this post a trophy
 		});
 	},
+	// Return true iff this user can give a trophy right now.
+	canTrophy: function () {
+		if (!Meteor.userId()) {
+			return true;
+		}
+		var startOfToday = new Date(Date.now());
+		startOfToday.setUTCMilliseconds(0);
+		startOfToday.setUTCSeconds(0);
+		startOfToday.setUTCMinutes(0);
+		// 09:00:00 UTC is 3am Pacific, which is when we reset trophies.
+		if (startOfToday.getUTCHours() < 11) {
+			startOfToday.setUTCDate(startOfToday.getUTCDate() - 1);
+		}
+		startOfToday.setUTCHours(11);
+		return Meteor.user().loft.lastTrophyTime < startOfToday.getTime();
+	},
+	// Get debug trophy info.
+	getDebugTrophy: function () {
+		var lastTrophyDate = new Date(Meteor.user().loft.lastTrophyTime);
+		var startOfToday = new Date(Date.now());
+		startOfToday.setUTCMilliseconds(0);
+		startOfToday.setUTCSeconds(0);
+		startOfToday.setUTCMinutes(0);
+		if (startOfToday.getUTCHours() < 11) {
+			startOfToday.setUTCDate(startOfToday.getUTCDate() - 1);
+		}
+		startOfToday.setUTCHours(11);
+		return lastTrophyDate.toLocaleString() + " (" + lastTrophyDate.getTime() + ")\n" +
+			startOfToday.toLocaleString() + " (" + startOfToday.getTime() + ")\n";
+	},
+	// Add a trophy for the given post.
 	addTrophy: function (postId) {
 		var post = posts.findOne(postId);
 		if (Meteor.userId() == post.userId) {
 			throw new Meteor.Error("Can't give a trophy to yourself.");
 		}
+		// TODO: check canTrophy()
 		posts.update(postId, { $addToSet: { trophiesBy: Meteor.userId() } });
+		Meteor.users.update(Meteor.userId(), { $set: { loft: { lastTrophyTime: Date.now() } } });
+		return true;
 	}
 });
