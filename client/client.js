c416f876d5e6a250a24707a2353338c0f083904f
Alexei Andreev <alexei.andreev@gmail.com>
2014-12-30 02:50:34 -0600

    Added new post animation feature.

--- client/client.js
+++ client/client.js
@@ -12,6 +12,7 @@ function init() {
 	Session.set("registerError", "");
 	Session.set("postsCount", 4);  // number of posts to show
 	Session.set("showUpdates", false);  // True iff we are showing updates section
+	Session.set("showingPostPopup", false);  // True iff we are showing the post popup modal
 	Session.set("selectedPost", undefined);  // Id of the post we have selected on the right
 	Session.set("selectedUpdate", undefined);  // Id of the update we have selected
 	Session.set("currentPage", Meteor.userId() ? PAGES.HOME : PAGES.WELCOME);
@@ -37,6 +38,12 @@ function init() {
 			console.log("getDebugInfo: " + err);
 		}
 	});
+}
+
+// Convert em value to px.
+function pxFromEm(input) {
+	var emSize = parseFloat($("body").css("font-size"));
+	return (input * emSize);
 }
 
 // Return cleaned and safe version of the given string.
@@ -80,7 +87,7 @@ Tracker.autorun(function () {
 	Meteor.subscribe("comments");
 });
 
-$(document).ready(function() {
+$(window).load(function() {
 	$(window).on("popstate", function(e) {
 		var state = e.originalEvent.state;
 		console.log("Popstate: " + JSON.stringify(state));
@@ -132,6 +139,98 @@ Template.home.helpers({
 	}
 });
 
+function showPostPopup() {
+	if (Session.get("showingPostPopup")) return;
+	Session.set("showingPostPopup", true);
+
+	var duration = 1000;
+	var promptDiv = $("#post-prompt");
+	var promptTextarea = promptDiv.find(".post-input-textarea");
+	var div = $("#post-popup");
+	var textarea = div.find(".post-input-textarea");
+
+	// Animate div.
+	var divFinalParams = {
+		left: div.position().left,
+		top: div.position().top,
+		width: div.width(),
+		height: div.height(),
+	};
+	div.css({
+		left: promptDiv.position().left,
+		top: promptDiv.position().top,
+		width: promptDiv.width(),
+		height: promptDiv.height(),
+	});
+	div.animate(divFinalParams, {duration: duration, queue: false});
+	div.css("visibility", "visible");
+
+	// Animate textarea.
+	var textareaFinalParams = {
+		fontSize: textarea.css("font-size"),
+		height: textarea.css("height"),
+	};
+	textarea.val(promptTextarea.val());
+	textarea.focus();
+	textarea.css({
+		fontSize: promptTextarea.css("font-size"),
+		height: promptTextarea.css("height"),
+	});
+	textarea.animate(textareaFinalParams, {duration: duration, queue: false, complete: function() {
+		// Set max-height so that it's set in pixels. Workaround for this bug:
+		// https://github.com/jackmoore/autosize/issues/191
+		var maxHeight = parseFloat(promptTextarea.css("max-height")) / 100.0;
+		textarea.css("max-height", div.height() * maxHeight);
+		textarea.autosize().trigger("autosize.resize");
+	}});
+
+	// Animate other stuff.
+	$("#darken").fadeTo(duration, 1);
+	div.find(".post-popup-footer").fadeTo(duration, 1);
+	promptDiv.css("visibility", "hidden");
+}
+
+function hidePostPopup() {
+	if (!Session.get("showingPostPopup")) return;
+
+	var duration = 1000;
+	var promptDiv = $("#post-prompt");
+	var promptTextarea = promptDiv.find(".post-input-textarea");
+	var div = $("#post-popup");
+	var textarea = div.find(".post-input-textarea");
+
+	// Animate div.
+	div.animate({
+		left: promptDiv.position().left,
+		top: promptDiv.position().top,
+		width: promptDiv.width(),
+		height: promptDiv.height(),
+	}, {duration: duration, queue: false});
+
+	// Animate textarea.
+	textarea.blur();
+	textarea.css("max-height", "");
+	//var oldHeight = textarea.height();
+	textarea.trigger("autosize.destroy");
+	//textarea.height(oldHeight);
+	textarea.animate({
+		fontSize: promptTextarea.css("font-size"),
+		height: promptTextarea.css("height"),
+	}, {duration: duration, queue: false, complete: function() {
+		div.removeAttr("style");
+		textarea.removeAttr("style");
+		promptDiv.css("visibility", "visible");
+		promptTextarea.val(textarea.val());
+		Session.set("showingPostPopup", false);
+	}});
+
+	// Animate other stuff.
+	$("#darken").fadeTo(duration, 0, function() {
+		$("#darken").hide();
+	});
+	div.find(".post-popup-footer").fadeTo(duration, 0);
+};
+
 Template.home.events({
 	"click #loft-logout": function (event) {
 		Meteor.logout();
@@ -166,15 +265,17 @@ Template.home.events({
 	"click #load-more": function (event) {
 		Session.set("postsCount", Session.get("postsCount") + 4);
 	},
-	"focus #post-input": function (event) {
-		// Set max-height so that it's set in pixels. Workaround for this bug:
-		// https://github.com/jackmoore/autosize/issues/191
-		var maxHeight = parseFloat($("#post-input").css("max-height")) / 100.0;
-		$("#post-input").css("max-height", $("#new-post-form").height() * maxHeight);
-		$("#post-input").autosize().trigger("autosize.resize");
+	"focus #post-prompt .post-input-textarea": function (event) {
+		showPostPopup();
 	},
-	"submit #new-post": function (event) {
-		Meteor.call("addPost", event.target.text.value, function(err, result) {
+	"click #darken": function (event) {
+		hidePostPopup();
+	},
+	"click #submit-post": function (event) {
+		var textarea = $("#post-popup .post-input-textarea");
+		var text = textarea.val();
+		console.log(text);
+		Meteor.call("addPost", textarea.val(), function(err, result) {
 			if (err == undefined) {
 				Session.set("postsLeft", Session.get("postsLeft") - 1);
 			} else {
@@ -182,11 +283,8 @@ Template.home.events({
 			}
 		});
 
-		// Clear form
-		event.target.text.value = "";
-		$("#post-input").trigger("autosize.resize");
-
-		// Prevent default form submit
+		textarea.val("");
+		hidePostPopup();
 		return false;
 	},
 	"submit .new-comment": function (event) {
@@ -202,7 +300,7 @@ Template.home.events({
 
 		// Prevent default form submit
 		return false;
-	}
+	},
 });
 
 // UPDATES
