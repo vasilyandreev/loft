303389f3758ff12ee0ea34ba1f9a7b95d8bfd626
Alexei Andreev <alexei.andreev@gmail.com>
2015-01-01 19:53:45 -0600

    Added extensions.js to put useful jquery extensions.

---
+++ client/extensions.js
@@ -0,0 +1,14 @@
+// JQuery extensions
+$.fn.setCursorPosition = function(pos) {
+	return this.each(function(index, elem) {
+		if (elem.setSelectionRange) {
+			elem.setSelectionRange(pos, pos);
+		} else if (elem.createTextRange) {
+			var range = elem.createTextRange();
+			range.collapse(true);
+			range.moveEnd('character', pos);
+			range.moveStart('character', pos);
+			range.select();
+		}
+	});
+};
